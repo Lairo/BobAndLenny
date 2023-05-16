10382f99ae0a67c77aa47686d14bf025bcd75b6e
chajus1 <chajus1@googlemail.com>
2023-05-17 00:24:38 +0100

    populated Program.cs

--- BobAndLenny/Program.cs
+++ BobAndLenny/Program.cs
@@ -10,6 +10,34 @@ namespace BobAndLenny
     {
         static void Main(string[] args)
         {
+
+            while (true)
+            {
+                Console.WriteLine("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy.ToLower() == "joe")
+                    {
+
+                    }
+                    else if{ whichGuy.ToLower() == "larry" }
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Larry'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
         }
     }
 }
